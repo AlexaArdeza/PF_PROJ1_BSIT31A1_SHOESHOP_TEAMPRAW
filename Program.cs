eb0d5ac928fe72ff5da5247f6cead9cf00aa51ca
John Andrei Guillena <andreiguillena2004@gmail.com>
2025-10-04 21:13:46 +0800

    fix: Bugs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using ShoeInventoryApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add Services
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") 
