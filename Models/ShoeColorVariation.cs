6fcd0a7ec1f310c742a4d389122a77a0191633fa
AlexaArdeza <C1435-23@itmlyceumalabang.onmicrosoft.com>
2025-10-12 14:45:40 +0800

    Add ShoeColorVariation model with validation and Shoe relationship

--- Models/ShoeColorVariation.cs
+++ Models/ShoeColorVariation.cs
@@ -1,11 +1,24 @@
-﻿namespace ShoeInventoryApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoeInventoryApp.Models
 {
     public class ShoeColorVariation
     {
+        [Key]
         public int Id { get; set; }
+
+        [ForeignKey("Shoe")]
         public int ShoeId { get; set; }
-        public Shoe Shoe { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Color { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be a non-negative number.")]
         public int Stock { get; set; }
+
+        // Navigation property
+        public Shoe Shoe { get; set; }
     }
 }
